9fa519b8c9bed7bcc40424d302fc229a28ea69a0
José Ulisses <julissesmacedo@outlook.com>
2024-01-19 22:21:50 -0300

    spliting toString method in two

--- Models/AppKey.cs
+++ Models/AppKey.cs
@@ -9,9 +9,27 @@ internal class AppKey
     [JsonPropertyName("value")]
     public string Value { get; set; }
 
-    public override string ToString()
+    public string ToConfig()
     {
         string output = $"<add key=\"{Key}\" value=\"{Value}\"/>";
+
+        return output;
+    }
+
+    public string ToJson()
+    {
+        int.TryParse(Value, out int intValue);
+        bool.TryParse(Value, out bool boolValue);
+        float.TryParse(Value, out float floatValue);
+
+        string output = $"\"{Key}\":";
+
+        if (Value.Equals(intValue.ToString()))
+            output += $"{intValue}";
+        else if (Value.Equals(boolValue.ToString(), StringComparison.InvariantCultureIgnoreCase))
+            output += $"{boolValue}";
+        else if (Value.Equals(floatValue.ToString()))
+            output += $"{floatValue}";
 
         return output;
     }
