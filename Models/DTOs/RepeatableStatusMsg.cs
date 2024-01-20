10b684848df8b0068e01f4f54e5ad2302532f284
José Ulisses <julissesmacedo@outlook.com>
2024-01-20 14:20:19 -0300

    new dto for run method of RepeatableStatus

---
+++ Models/DTOs/RepeatableStatusMsg.cs
@@ -0,0 +1,9 @@
+﻿namespace config.Models.DTOs;
+internal class RepeatableStatusMsg
+{
+    public string InitalMsg { get; set; }
+
+    public string RepeatableMsg { get; set; }
+
+    public string FinalMsg { get; set; }
+}
