90325f938a1e061eaed9919c192c8d880cf2c97e
José Ulisses <julissesmacedo@outlook.com>
2024-01-19 22:21:37 -0300

    new command options for appsettings

--- Settings/AppSettingsSettings.cs
+++ Settings/AppSettingsSettings.cs
@@ -1,6 +1,14 @@
-﻿using Spectre.Console.Cli;
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
 
 namespace config.Settings;
 internal class AppSettingsSettings : CommandSettings
 {
+    [CommandOption("-s|--select")]
+    [DefaultValue(false)]
+    public bool SelectKeys { get; set; }
+
+    [CommandOption("-j|--json")]
+    [DefaultValue(false)]
+    public bool Json { get; set; }
 }
