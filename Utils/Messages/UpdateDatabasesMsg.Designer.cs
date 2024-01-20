8494af9d416477f2092c17d50c810f584349a688
José Ulisses <julissesmacedo@outlook.com>
2024-01-20 14:14:49 -0300

    moving messages for utils\messages

--- Utils/Messages/UpdateDatabasesMsg.Designer.cs
+++ Utils/Messages/UpdateDatabasesMsg.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace config.Messages {
+namespace config.Utils.Messages {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace config.Messages {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("config.Messages.UpdateDatabasesMsg", typeof(UpdateDatabasesMsg).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("config.Utils.Messages.UpdateDatabasesMsg", typeof(UpdateDatabasesMsg).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
