c35752f61bc0bdd7eb10a5a65a77bfad0c875ddd
José Ulisses <julissesmacedo@outlook.com>
2024-01-20 14:19:31 -0300

    refatory appSettings structure and implemented new method for update file

---
+++ Models/AppSettingsGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace config.Models;
+internal class AppSettingsGroup
+{
+    [JsonPropertyName("group")]
+    public string GroupName { get; set; }
+
+    [JsonPropertyName("keys")]
+    public List<AppKey> Keys { get; set; }
+}
--- Singleton/AppSettingsSingleton.cs
+++ Singleton/AppSettingsSingleton.cs
@@ -1,34 +1,42 @@
 ﻿using config.Models;
+
 using System.Text.Json;
 
 namespace config.Singleton
 {
-	internal class AppSettingsSingleton
+    internal class AppSettingsSingleton
     {
-		private static AppSettingsSingleton _instance { get; set; }
+        private static AppSettingsSingleton _instance { get; set; }
 
-		private static string Path = @"./Data/AppSettings.json";
+        private static string Path = @"./Data/AppSettings.json";
 
-		private AppSettings AppSettings { get; set; }
+        private List<AppSettingsGroup> AppSettings { get; set; }
 
-		private JsonSerializerOptions Options = new () { WriteIndented = true };
+        private JsonSerializerOptions Options = new() { WriteIndented = true };
 
-		public static AppSettingsSingleton Instance
-		{
-			get
-			{
+        public static AppSettingsSingleton Instance
+        {
+            get
+            {
                 _instance ??= new AppSettingsSingleton();
 
-				return _instance;
-			}
-		}
+                return _instance;
+            }
+        }
 
-		public AppSettings Lines()
-		{
-			var text = File.ReadAllText("D:\\study\\my\\console\\config\\Data\\AppSettings.json");
-			AppSettings = JsonSerializer.Deserialize<AppSettings>(text)!;
+        public List<AppSettingsGroup> Lines()
+        {
+            var text = File.ReadAllText("D:\\study\\my\\console\\config\\Data\\AppSettings.json");
+            AppSettings = JsonSerializer.Deserialize<List<AppSettingsGroup>>(text)!;
 
-			return AppSettings;
-		}
-	}
+            return AppSettings;
+        }
+
+        public void Update(List<AppSettingsGroup> appSettings)
+        {
+            var json = JsonSerializer.Serialize(appSettings, Options);
+            File.WriteAllText("D:\\study\\my\\console\\config\\Data\\AppSettings.json", json);
+            AppSettings = appSettings;
+        }
+    }
 }
