63ad5df25c7b34f1773fd8c571c437ad451d0910
José Ulisses <julissesmacedo@outlook.com>
2024-01-21 13:46:31 -0300

    intial files for new command

---
+++ Commands/AppSettings/RemoveKeyCommand.cs
@@ -0,0 +1,11 @@
+﻿using config.Settings.AppSettings;
+using Spectre.Console.Cli;
+
+namespace config.Commands.AppSettings;
+internal class RemoveKeyCommand : Command<RemoveKeySettings>
+{
+    public override int Execute(CommandContext context, RemoveKeySettings settings)
+    {
+        throw new NotImplementedException();
+    }
+}
