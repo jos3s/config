d5d2a4812cd00f065aad30773707a4ee4b77d807
José Ulisses <julissesmacedo@outlook.com>
2024-01-21 16:42:53 -0300

    breaks methods from createline in new methods

--- Utils/CreateLine.cs
+++ Utils/CreateLine.cs
@@ -1,12 +1,11 @@
 ﻿using config.Models;
-using Spectre.Console;
 using System.Text;
 
 namespace config.Utils
 {
-	internal class CreateLine
+    internal class CreateLine
 	{
-		public static string Config(List<ConnectionLine> connectionLines, string user, string password, string instance)
+		public static string Config(IEnumerable<ConnectionLine> connectionLines, string user, string password, string instance)
 		{
 			StringBuilder stringBuilder = new();
 
@@ -15,31 +14,36 @@ namespace config.Utils
 			var passwordString = $"[darkseagreen4_1]User ID={password}[/];";
 
 			foreach (var connectionLine in connectionLines)
-			{
-				stringBuilder.Append("<add ");
-
-				stringBuilder.Append($"[skyblue2]Name=\"{connectionLine.Name}\"[/] ");
-				stringBuilder.Append($"providerName=\"{connectionLine.ProviderName}\" ");
-
-				stringBuilder.Append("connectionString=\"");
-				stringBuilder.Append(instanceString);
-				stringBuilder.Append($"Initial Catalog={connectionLine.ConnectionString.InitalCatalog};");
-				stringBuilder.Append(userIdString);
-				stringBuilder.Append(passwordString);
-				stringBuilder.Append($"Pooling={connectionLine.ConnectionString.Pooling};");
-				stringBuilder.Append($"Connect Timeout={connectionLine.ConnectionString.ConnectTimeout};");
-				stringBuilder.Append($"Application Name={connectionLine.ConnectionString.AplicationName}");
-				stringBuilder.Append('"');
-
-				stringBuilder.Append("/>");
-				stringBuilder.Append(Environment.NewLine);
-			}
+            {
+                stringBuilder.AppendLine(ConfigLine(connectionLine, instanceString, userIdString, passwordString));
+            }
 
 			return stringBuilder.ToString();
 		}
 
+        public static string ConfigLine(ConnectionLine connectionLine, string instanceString, string userIdString, string passwordString)
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append("<add ");
 
-		public static string Json(List<ConnectionLine> connectionLines, string user, string password, string instance)
+            stringBuilder.Append($"[skyblue2]Name=\"{connectionLine.Name}\"[/] ");
+            stringBuilder.Append($"providerName=\"{connectionLine.ProviderName}\" ");
+
+            stringBuilder.Append("connectionString=\"");
+            stringBuilder.Append(instanceString);
+            stringBuilder.Append($"Initial Catalog={connectionLine.ConnectionString.InitalCatalog};");
+            stringBuilder.Append(userIdString);
+            stringBuilder.Append(passwordString);
+            stringBuilder.Append($"Pooling={connectionLine.ConnectionString.Pooling};");
+            stringBuilder.Append($"Connect Timeout={connectionLine.ConnectionString.ConnectTimeout};");
+            stringBuilder.Append($"Application Name={connectionLine.ConnectionString.AplicationName}");
+            stringBuilder.Append('"');
+
+            stringBuilder.Append("/>");
+			return stringBuilder.ToString();
+        }
+
+        public static string Json(IEnumerable<ConnectionLine> connectionLines, string user, string password, string instance)
 		{
 
 			var instanceString = $"Data Source={instance};";
@@ -50,24 +54,30 @@ namespace config.Utils
 
 			foreach (var connectionLine in connectionLines)
 			{
-				stringBuilder.Append($"\"{connectionLine.Name}\":");
-
-				stringBuilder.Append("\"");
-				stringBuilder.Append(instanceString);
-				stringBuilder.Append($"Initial Catalog={connectionLine.ConnectionString.InitalCatalog};");
-				stringBuilder.Append(userIdString);
-				stringBuilder.Append(passwordString);
-				stringBuilder.Append($"Pooling={connectionLine.ConnectionString.Pooling};");
-				stringBuilder.Append($"Connect Timeout={connectionLine.ConnectionString.ConnectTimeout};");
-				stringBuilder.Append($"Application Name={connectionLine.ConnectionString.AplicationName}");
-				stringBuilder.Append('"');
-
-				stringBuilder.Append(",");
-
-				stringBuilder.Append(Environment.NewLine);
-			}
+                stringBuilder.AppendLine(JsonLine(connectionLine, instanceString, userIdString, passwordString));
+            }
 
 			return stringBuilder.ToString();
 		}
-	}
+
+        public static string JsonLine(ConnectionLine connectionLine, string instanceString, string userIdString, string passwordString)
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append($"\"{connectionLine.Name}\":");
+
+            stringBuilder.Append("\"");
+            stringBuilder.Append(instanceString);
+            stringBuilder.Append($"Initial Catalog={connectionLine.ConnectionString.InitalCatalog};");
+            stringBuilder.Append(userIdString);
+            stringBuilder.Append(passwordString);
+            stringBuilder.Append($"Pooling={connectionLine.ConnectionString.Pooling};");
+            stringBuilder.Append($"Connect Timeout={connectionLine.ConnectionString.ConnectTimeout};");
+            stringBuilder.Append($"Application Name={connectionLine.ConnectionString.AplicationName}");
+            stringBuilder.Append('"');
+
+            stringBuilder.Append(",");
+            return stringBuilder.ToString();
+        }
+
+    }
 }
