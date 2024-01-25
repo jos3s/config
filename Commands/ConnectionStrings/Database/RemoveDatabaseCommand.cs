fbb7f93c48b5811add344f2cb7e41c69bd43b2c4
José Ulisses <julissesmacedo@outlook.com>
2024-01-24 21:54:57 -0300

    create RemoveDatabaseCommand

---
+++ Commands/ConnectionStrings/Database/RemoveDatabaseCommand.cs
@@ -0,0 +1,49 @@
+﻿using config.Models.DTOs;
+using config.Settings.ConnectionStrings.Database;
+using config.Singleton;
+using config.Utils.Display;
+using config.Utils.Messages;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace config.Commands.ConnectionStrings.Database;
+internal class RemoveDatabaseCommand : Command<RemoveDatabaseSettings>
+{
+    public override int Execute(CommandContext context, RemoveDatabaseSettings settings)
+    {
+        var databases = ConnectionStringsSingleton.Instance.Lines();
+
+        if (databases.Count == 0)
+        {
+            AnsiConsole.Write("Not databases to remove.");
+            return 0;
+        }
+        
+        var databasesNames = databases.Select(x => x.Name);
+
+        var databaseSelected = SelectionDisplay.Selection(databasesNames, "database");
+
+        if (AnsiConsole.Confirm($"Remove {databaseSelected} database?", false))
+        {
+            var databaseSelectedObject = databases.FirstOrDefault(x=> x.Name.Equals(databaseSelected, StringComparison.CurrentCultureIgnoreCase));
+
+            databases.Remove(databaseSelectedObject);
+
+            ConnectionStringsSingleton.Instance.Update(databases);
+
+            RepeatableStatusDisplay.Run(new RepeatableStatusMsg
+            {
+                InitalMsg = DatabasesMsg.INF006,
+                RepeatableMsg = DatabasesMsg.INF007,
+                FinalMsg = DatabasesMsg.INF008
+            }, 2);
+
+        }
+        else
+        {
+            AnsiConsole.Write("Database not removed.");
+        }
+
+        return 0;
+    }
+}
