e8bdf4bf9a7e1e1b7ed829cd15cd68a1983180be
José Ulisses <julissesmacedo@outlook.com>
2024-01-24 21:54:04 -0300

    new method to SelectionDisplay

--- Utils/Display/SelectionDisplay.cs
+++ Utils/Display/SelectionDisplay.cs
@@ -16,4 +16,15 @@ internal class SelectionDisplay
        
         return AnsiConsole.Prompt(selectionPrompt);
     }
+
+    public static string Selection(IEnumerable<string> options, string nameOfOptions)
+    {
+        var selectionPrompt = new SelectionPrompt<string>()
+            .Title($"Select [green]{nameOfOptions}[/]:")
+            .PageSize(options.Count() > 3? options.Count() : 3)
+            .MoreChoicesText($"[grey](Move up and down to reveal more {nameOfOptions})[/]")
+            .AddChoices(options);
+       
+        return AnsiConsole.Prompt(selectionPrompt);
+    }
 }
