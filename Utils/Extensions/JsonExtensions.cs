bbede6c9687c47a92c28ed4c2377a507ba5ad2fa
José Ulisses <julissesmacedo@outlook.com>
2024-01-26 22:49:28 -0300

    new extension method for JsonText

---
+++ Utils/Extensions/JsonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+using Spectre.Console.Json;
+
+namespace config.Utils.Extensions;
+internal static class JsonExtensions
+{
+    public static JsonText StyledJsonText(this JsonText text)
+    {
+        text.BracesColor(Color.Grey84)
+            .BracketColor(Color.Grey84)
+            .ColonColor(Color.Yellow)
+            .CommaColor(Color.Grey84)
+            .StringColor(Color.Green)
+            .NumberColor(Color.Blue)
+            .BooleanColor(Color.Red)
+            .NullColor(Color.Green).MemberColor(Color.White);
+        return text;
+    }
+}
