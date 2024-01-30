a06da928d85279818779a1cd787190ad006c7892
José Ulisses <julissesmacedo@outlook.com>
2024-01-30 19:54:03 -0300

    new extensions for SpectreConsole

---
+++ Utils/Extensions/SpectreConsoleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console;
+
+namespace config.Utils.Extensions;
+internal static class SpectreConsoleExtensions
+{
+    public static Panel Formatted(this Panel panel)
+    {
+        return panel.Border(BoxBorder.Rounded)
+                    .Padding(2, 2, 2, 2)
+                    .Expand();
+    }
+}
