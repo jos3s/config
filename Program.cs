29e2449d80ff75d221645a47dc5c585d23851508
José Ulisses <julissesmacedo@outlook.com>
2024-02-01 23:04:44 -0300

    exception handler

--- Program.cs
+++ Program.cs
@@ -1,20 +1,45 @@
 ﻿using config.Branchs;
+using config.Exceptions;
+using config.Utils.Extensions;
+using config.Utils.Messages.Documentation;
+
 using Spectre.Console;
 using Spectre.Console.Cli;
 
-var app = new CommandApp();
-
-app.Configure(config =>
-{
-	config
-        .UseConnectionStringsBranch()
-        .UseAppSettingsBranch()
-        .UseDataBranch();
-});
-
 try
 {
-	app.Run(args);
+    var app = new CommandApp();
+
+    app.Configure(config =>
+    {
+        config
+            .UseConnectionStringsBranch()
+            .UseAppSettingsBranch()
+            .UseDataBranch();
+
+        config.SetExceptionHandler((ex) =>
+        {
+            if(ex is FileException)
+            {
+                var texts = new List<Text>
+                {
+                    new ($"{ex.Message}",new Style(Color.Red)),
+                    new ($"Execute 'config {DocumentationMsg.BRANCH001} {DocumentationMsg.COMMAND007}' to create configuration files.")
+                };
+
+                new Panel(new Rows(texts))
+                    .Formatted()
+                    .Write();
+            
+            }
+            else
+            {
+                AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+            }
+        });
+    });
+
+    app.Run(args);
 }
 catch (Exception ex)
 {
