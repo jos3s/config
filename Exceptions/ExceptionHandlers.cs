a059cdc5dfcd960b45c9c41b690d3fa918be4eac
José Ulisses <julissesmacedo@outlook.com>
2024-02-03 00:35:39 -0300

    new exception handler

---
+++ Exceptions/ExceptionHandlers.cs
@@ -0,0 +1,26 @@
+﻿using config.Utils.Extensions;
+using config.Utils.Messages.Documentation;
+
+using Spectre.Console;
+
+namespace config.Exceptions;
+internal static class ExceptionHandlers
+{
+    public static void Handler(Exception ex)
+    {
+        if (ex is FileException)
+        {
+            var texts = new List<Text>
+                {
+                    new ($"{ex.Message}",new Style(Color.Red)),
+                    new ($"Execute 'config {DocumentationMsg.BRANCH001} {DocumentationMsg.COMMAND007}' to create configuration files.")
+                };
+
+            new Panel(new Rows(texts)).Formatted().Write();
+        }
+        else
+        {
+            AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using config.Branchs;
 using config.Exceptions;
-using config.Utils.Extensions;
-using config.Utils.Messages.Documentation;
 
 using Spectre.Console;
 using Spectre.Console.Cli;
@@ -17,26 +15,7 @@ try
             .UseAppSettingsBranch()
             .UseDataBranch();
 
-        config.SetExceptionHandler((ex) =>
-        {
-            if(ex is FileException)
-            {
-                var texts = new List<Text>
-                {
-                    new ($"{ex.Message}",new Style(Color.Red)),
-                    new ($"Execute 'config {DocumentationMsg.BRANCH001} {DocumentationMsg.COMMAND007}' to create configuration files.")
-                };
-
-                new Panel(new Rows(texts))
-                    .Formatted()
-                    .Write();
-            
-            }
-            else
-            {
-                AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
-            }
-        });
+        config.SetExceptionHandler(ExceptionHandlers.Handler);
     });
 
     app.Run(args);
